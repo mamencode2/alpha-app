a35bd191ffc81a63ce1e4a8afd466ea388f79a1e
mamencode2 <mamencode2@gmail.com>
2023-08-23 22:18:08 +0000

    vbdsf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react"
 
+import {QuizContextProvider}from './contexts/QuizContextProvider'
+
 export default function App(){
   return(
+    <QuizContextProvider>
     <div>
+
       
       <h1>hello world11 uuuuyyyyyyyyy</h1>
       <Qheader
       />
     </div>
+    </QuizContextProvider>
   )
 }
 
