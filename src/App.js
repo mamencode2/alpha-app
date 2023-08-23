5376cb09b1f71ea908be5eab07ea68239bcb9115
mamencode2 <mamencode2@gmail.com>
2023-08-23 21:52:27 +0000

    vcbghu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { QuizContextProvider } from "./contexts/QuizContextProvider"
 
 export default function App(){
   return(
     <div>
       
-      <h1>hello world11</h1>
+      <h1>hello world11 uuuuyyyyyyyyy</h1>
       
     </div>
   )
